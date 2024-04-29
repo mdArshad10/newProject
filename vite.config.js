56ef0b155bc3b880d23b7751c09f61052845b1d7
mdArsahd10 <mdarshd246@gmail.com>
2024-04-29 10:46:06 +0530

    fixing the page navigation

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,25 @@
 import path, { resolve } from "path";
 import { partytownVite } from "@builder.io/partytown/utils";
-import { defineConfig } from "vite";
+import { build, defineConfig } from "vite";
 
 // vite.config.js
 
-export default defineConfig({
-  base: "/",
-
-
+export default {
   plugins: [
-    // html(),
     partytownVite({
       dest: path.join(__dirname, "dist", "~partytown"),
     }),
   ],
-});
+
+  build:{
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        ascut: resolve(__dirname,"pages/ascut/index.html"),
+        retail_register: resolve(__dirname,"pages/retail_register/index.html"),
+        thank_you: resolve(__dirname,"pages/thank_you/index.html"),
+        process_pages: resolve(__dirname,"pages/process_pages/index.html"),
+      },
+    },
+  }
+};
