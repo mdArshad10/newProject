ab004a4e5b819eef01c72512cdc0a2c64a2a3fb6
mdArsahd10 <mdarshd246@gmail.com>
2024-04-29 10:53:47 +0530

    fixing the vite entry point challenge

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,26 @@
 import path, { resolve } from "path";
 import { partytownVite } from "@builder.io/partytown/utils";
-import { build, defineConfig } from "vite";
+import html from "vite-plugin-html";
 
 // vite.config.js
 
 export default {
   plugins: [
+    html(),
     partytownVite({
       dest: path.join(__dirname, "dist", "~partytown"),
     }),
   ],
 
-  build:{
+  build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        ascut: resolve(__dirname,"pages/ascut/index.html"),
-        retail_register: resolve(__dirname,"pages/retail_register/index.html"),
-        thank_you: resolve(__dirname,"pages/thank_you/index.html"),
-        process_pages: resolve(__dirname,"pages/process_pages/index.html"),
+        ascut: resolve(__dirname, "pages/ascut/index.html"),
+        retail_register: resolve(__dirname, "pages/retail_register/index.html"),
+        thank_you: resolve(__dirname, "pages/thank_you/index.html"),
+        process_pages: resolve(__dirname, "pages/process_pages/index.html"),
       },
     },
-  }
+  },
 };
